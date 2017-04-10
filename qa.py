abe54a6d97546f68e8df7896713b2572fd394869
Allen <arlu@andrew.cmu.edu>
2017-04-10 02:00:35 -0400

    Created question generation system

--- qa.py
+++ qa.py
@@ -1,17 +1,37 @@
 import nltk
 import string
 from bllipparser import RerankingParser
+import re
 
+#Python 3
 # download and install a basic unified parsing model (Wall Street Journal)
 # sudo python -m nltk.downloader bllip_wsj_no_aux
 model_dir = nltk.data.find('models/bllip_wsj_no_aux').path
 rrp = RerankingParser.from_unified_model_dir(model_dir)
 
+def textToSentences(text):
+	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+	return tokenizer.tokenize(text)
+
 def getTags(sentence):
 	return nltk.pos_tag(nltk.word_tokenize(sentence))
 
+def scoredParseToTree(scored_parse):
+	return nltk.tree.Tree.fromstring(str(scored_parse.ptb_parse))
+
 def getParseTree(sentence):
-	return rrp.simple_parse(sentence)
+	words = []
+	tag_map = {}
+	for i,(word,tag) in enumerate(getTags(sentence)):
+		words.append(word)
+		if tag != None:
+			tag_map[i] = tag
+
+	nbest_list = rrp.parse_tagged(words,tag_map)
+
+	for scored_parse in nbest_list:
+		#return best parse tree
+		return re.sub('\s+', ' ',str(scoredParseToTree(scored_parse))).strip()
 
 class Tree(object):
 	def __init__(self):
@@ -60,4 +80,92 @@ def makeTree(parsed):
 def treeFromSentence(sentence):
 	parsed = getParseTree(sentence)
 	return makeTree(parsed)
+
+def generateQuestions(text):
+	sentences = textToSentences(text)
+	questions = []
+	for sentence in sentences:
+		parts = sentence.split(',')
+		for part in parts:
+			t = treeFromSentence(part)
+			parsed = t.child_tags
+			if len(parsed) > 1:
+				first = parsed[0]
+				second = parsed[1]
+				if first[0] == 'NP' and second[0] == 'VP':
+					question = questionNPVP(first[1],second[1])
+					if question != None:
+						questions.append(question)
+					else:
+						parsed_vp = t.children[1].child_tags
+						if len(parsed_vp) > 1:
+							vp_first = parsed_vp[0]
+							vp_second = parsed_vp[1]
+							if vp_first[0].startswith('VB') and vp_second[0] == 'NP':
+								questions.append('What ' + vp_first[1] + ' ' +
+												vp_second[1] + '?')
+
+			elif len(parsed) > 2:
+				first = parsed[0]
+				second = parsed[1]
+				third = parsed[2]
+				if second[0].startswith('VB') and third[0] == 'NP':
+					questions.append('What ' + second[1] + ' ' + third[1] + '?')
+	return questions
+
+def isPerson(np):
+	np_tags = getTags(np)
+	if np_tags[0][0] == 'PRP':
+		return True
+	else:
+		entities = nltk.chunk.ne_chunk(np_tags)
+		for entity in entities:
+			if isinstance(entity,nltk.tree.Tree) and entity._label == 'PERSON':
+				return True
+	return False	
+
+def questionNPVP(np,vp):
+	if isPerson(np):
+		return 'Who ' + vp + '?'
+	elif np != 'It':
+		return 'What ' + vp + '?'
+	return None
+
+'''
+def writeRuleHelp(preterminal,t,rules):
+	if len(t.children) == 0:
+		rhs = ' -> ' + t.sentence
+		if not t.tag in rules:
+			rules[t.tag] = {rhs:1}
+		elif not rhs in rules[t.tag]:
+			rules[t.tag][rhs] = 1
+		else:
+			rules[t.tag][rhs] += 1
+		return
+	rhs = ' -> ' + ' '.join([tag[0] for tag in t.child_tags])
+	if not preterminal in rules:
+		rules[preterminal] = {rhs:1}
+	elif not rhs in rules[preterminal]:
+		rules[preterminal][rhs] = 1
+	else:
+		rules[preterminal][rhs] += 1
+	for child in t.children:
+		writeRuleHelp(child.tag,child,rules)
+
+def writeRules(file_in,file_out):
+	f = open(file_in)
+	arr = f.readlines()
+	f.close()
+
+	rules = {'ROOT': {}}
+	for i in range(len(arr)):
+		rules['ROOT'][' -> S1'] = 1
+		writeRuleHelp('S1',treeFromSentence(arr[i]),rules)
+	f = open(file_out,'w+')
+	for key in rules.keys():
+		for rhs in rules[key].keys():
+			count = rules[key][rhs]
+			f.write(key + rhs + ' ' + str(count) + '\n')
+	f.close()
+'''
 			
